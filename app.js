254ca159a794e02825ba07d17c9a44687db100d9
Lazar Narcis <lnarcis310@gmail.com>
2022-06-04 09:13:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let questions = ["Who created Facebook?", "Who is the richest man in the world?"];
+
+let responsesQ1 = {
+    "Bill Gates": false,
+    "Mark Zukerberg": true,
+    "Jeff Bezos": false,
+    "Daniel Sam": false
+};
+
+let responsesQ2 = {
+    "Bill Gates": false,
+    "Mark Zukerberg": false,
+    "Jeff Bezos": false,
+    "Elon Musk": true
+};
