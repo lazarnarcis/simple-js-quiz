9773d7ba2132bd8216354e9a0989dd97e4964c89
Lazar Narcis <lnarcis310@gmail.com>
2022-06-04 09:18:27 +0300

    we arrange things more correctly to help us in the future

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-let questions = ["Who created Facebook?", "Who is the richest man in the world?"];
-
-let responsesQ1 = {
-    "Bill Gates": false,
-    "Mark Zukerberg": true,
-    "Jeff Bezos": false,
-    "Daniel Sam": false
-};
-
-let responsesQ2 = {
-    "Bill Gates": false,
-    "Mark Zukerberg": false,
-    "Jeff Bezos": false,
-    "Elon Musk": true
-};
+let questions = [
+    {
+        text: "Who created Facebook?",
+        answers: {
+            "Bill Gates": false,
+            "Mark Zukerberg": true,
+            "Jeff Bezos": false,
+            "Daniel Sam": false
+        }
+    }, 
+    {
+        text: "Who is the richest man in the world?",
+        answers: {
+            "Bill Gates": false,
+            "Mark Zukerberg": false,
+            "Jeff Bezos": false,
+            "Elon Musk": true
+        }
+    },
+];
 
 let quiz = document.querySelector("#quiz");
 
 for (let i = 0; i < questions.length; i++) {
     let question = document.createElement("p");
-    question.innerText = questions[i];
+    question.innerText = questions[i].text;
     quiz.appendChild(question);
+    
 }
